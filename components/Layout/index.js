cf36efc9a11098fac00d364b154d21547874e4fc
Cameron Milsom <cameron.live@gmail.com>
2024-07-25 22:03:27 +0100

    Added in basic routing.

---
+++ components/Layout/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Head from "next/head";
+import Header from "@components/Header/Header";
+import Footer from "@components/Footer/Footer";
+
+import styles from "./Layout.module.css";
+
+const Layout = ({ children }) => {
+  return (
+    <div className={styles.root}>
+      <Head>
+        <title>Catherine Jeynes Photography</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+
+      <Header />
+      <main className={styles.main}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,14 @@
-import Head from "next/head";
-import Header from "@components/Header/Header";
-import Footer from "@components/Footer/Footer";
+import React from "react";
+import styles from "./Home.module.css";
 
-import styles from "./index.module.css";
-
-export default function Home() {
+const Home = () => {
   return (
-    <div className={styles.root}>
-      <Head>
-        <title>Catherine Jeynes Photography</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-
-      <Header />
-      <main className={styles.main}>
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
-      <Footer />
-    </div>
+    <main className={styles.main}>
+      <p className="description">
+        Get started by editing <code>pages/index.js</code>
+      </p>
+    </main>
   );
-}
+};
+
+export default Home;
