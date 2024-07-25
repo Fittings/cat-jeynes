a2b647c103a0a3837c0ca3e10de4ca8ba5f8f13d
Cameron Milsom <cameron.live@gmail.com>
2024-07-25 22:21:49 +0100

    Added in a contact form.

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Contact = () => {
-  return <div>CONTACT ME</div>;
+  return (
+    <div>
+      testing forms
+      <form name="contact" method="POST" netlify>
+        <p>
+          <label>
+            Name <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Email <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Contact;
