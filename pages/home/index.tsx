57217385bfcd5ee38e462a6ab77855f34d2ffd46
Cameron Milsom <cameron.live@gmail.com>
2024-07-30 22:30:54 +0100

    Fixed rendering issues.

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React, { HTMLProps } from "react";
 import Grid from "./Grid";
 import styles from "./styles.module.scss";
+import Head from "next/head";
 
 const Image = ({ ...rest }) => {
   return (
@@ -19,14 +20,14 @@ const Image = ({ ...rest }) => {
 const Home = () => {
   return (
     <div className={styles.home}>
-      <head>
+      <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Inria+Serif:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Petemoss&display=swap"
           rel="stylesheet"
         />
-      </head>
+      </Head>
       <div className={styles.hero}>
         <img src="/images/mountains.jpg" />
       </div>
